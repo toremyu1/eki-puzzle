52da07ed6fda74feb15578ee1498729ebea9e083
Nimuro-1234 <rakutentrain074@gmail.com>
2026-06-09 21:14:06 +0900

    Update extract_stations.py

--- extract_stations.py
+++ extract_stations.py
@@ -206,7 +206,7 @@ def get_todays_sub_pages():
         chunks[weekday] = ["あ"]
 
     # ★テスト用に「あ」のページだけを強制的に指定します
-    return ["しや-しん"], weekday
+    # return ["しや-しん"], weekday
 
     return chunks[weekday], weekday
 
