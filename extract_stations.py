0c4cbb770f5137e5be7e458f44f5a5545342d0e4
Nimuro-1234 <rakutentrain074@gmail.com>
2026-06-09 16:28:34 +0900

    Update extract_stations.py

--- extract_stations.py
+++ extract_stations.py
@@ -195,7 +195,7 @@ def get_todays_sub_pages():
     weekday = datetime.datetime.today().weekday()
 
     # ★動作テストのため、強制的に「6:日曜日」に設定します
-    weekday = 1
+    weekday = 2
 
     # 取得した全ページを曜日ごとに自動で7等分する
     chunks = [[] for _ in range(7)]
